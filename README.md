f2215b323941a0df0e3ced2c67d10e2709bd208c
Kos <koshuang@gmail.com>
2015-09-12 21:08:17 +0800

    add more packages

--- README.md
+++ README.md
@@ -15,10 +15,23 @@ curl -s https://raw.githubusercontent.com/koshuang/mac_apps/master/install.sh | 
 * Skype http://www.skype.com/go/getskype-macosx.dmg
 * Dropbox https://www.dropbox.com/download?plat=mac
 * Sublime Text http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%20Build%203065.dmg
+* Atom https://atom.io/download/mac
 * SourceTree http://downloads.atlassian.com/software/sourcetree/SourceTree_2.0.5.2.dmg
 * Evernote http://evernote.com/download/get.php?file=EvernoteMac
 * FileZilla http://downloads.sourceforge.net/project/filezilla/FileZilla_Client/3.9.0.6/FileZilla_3.9.0.6_macosx-x86.app.tar.bz2
 * Docker Toolbox https://github.com/docker/toolbox/releases/download/v1.8.2/DockerToolbox-1.8.2.pkg
+* iTerm https://iterm2.com/downloads/stable/iTerm2-2_1_1.zip
+* Slate http://slate.ninjamonkeysoftware.com/Slate.dmg
+* Homebrew
+* Install by brew
+  * rvm
+  * nvm
+  * tree
+  * watch
+  * wget
+  * tmux
+  * zsh & oh my sh
+  * vim & spf13
 
 
 ## How to contribute
--- install.sh
+++ install.sh
@@ -8,15 +8,13 @@ function install_app_from_dmg {
   app_folder="/Applications/$app.app"
   volumn_path="/Volumes/$volumn_name"
 
-  if [ -d "$app_folder" ]
-  then
+  if [ -d "$app_folder" ]; then
     echo "$app aleady installed."
   else
     echo "Installing $app"
 
     # download dmg if it is not existed
-    if [ -e "$dmg_file" ]
-    then
+    if [ -e "$dmg_file" ]; then
       echo "$dmg_file existed"
     else
       echo "download $dmg_file"
@@ -37,15 +35,13 @@ function install_app_from_bz {
   bz_file="$app.app.tar.bz2"
   app_folder="/Applications/$app.app"
 
-  if [ -d "$app_folder" ]
-  then
+  if [ -d "$app_folder" ]; then
     echo "$app aleady installed."
   else
     echo "Installing $app"
 
     # download dmg if it is not existed
-    if [ -e "$bz_file" ]
-    then
+    if [ -e "$bz_file" ]; then
       echo "$bz_file existed"
     else
       echo "download $bz_file"
@@ -65,15 +61,13 @@ function install_app_from_pkg {
   pkg_file="$app.pkg"
   app_folder="/Applications/$app"
 
-  if [ -d "$app_folder" ]
-  then
+  if [ -d "$app_folder" ]; then
     echo "$app aleady installed."
   else
     echo "Installing $app"
 
     # download dmg if it is not existed
-    if [ -e "$pkg_file" ]
-    then
+    if [ -e "$pkg_file" ]; then
       echo "$pkg_file existed"
     else
       echo "download $pkg_file"
@@ -86,7 +80,56 @@ function install_app_from_pkg {
   fi
 }
 
+function install_homebrew {
+
+  if hash brew 2>/dev/null; then
+    echo homebrew already installed.
+  else
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  fi
+}
+
+function install_by_homebrew {
+  app=$1
+
+  if brew list -1 | grep -q "^${app}\$"; then
+    echo $app already installed.
+  else
+    brew install "$app"
+    echo "$app has been installed."
+  fi
+}
+
+function install_oh_my_zsh {
+  if [ -d ~/.oh-my-zsh ]; then
+    echo oh_my_zsh already installed.
+  else
+    sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    echo oh_my_zsh has been installed.
+  fi
+}
+
+function install_spf13 {
+  if [ -e ~/.vimrc.local ]; then
+    echo spf13 already installed.
+  else
+    curl https://j.mp/spf13-vim3 -L > spf13-vim.sh && sh spf13-vim.sh
+    echo spf13 has been installed.
+  fi
+}
+
+function install_rvm {
+  if [ -e ~/.rvm ]; then
+    echo rvm already installed.
+  else
+    \curl -sSL https://get.rvm.io | bash -s stable
+    echo rvm has been installed.
+  fi
+}
+
 cd ~/Downloads
+
+install_homebrew
 
 install_app_from_dmg "Google Chrome" "https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg"
 install_app_from_dmg "FirefoxNightly" "https://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-trunk/firefox-43.0a1.en-US.mac.dmg" "Nightly"
@@ -97,6 +140,27 @@ install_app_from_dmg "Sublime Text" "http://c758482.r82.cf2.rackcdn.com/Sublime%
 install_app_from_dmg "SourceTree" "http://downloads.atlassian.com/software/sourcetree/SourceTree_2.0.5.2.dmg"
 install_app_from_dmg "Evernote" "http://evernote.com/download/get.php?file=EvernoteMac"
 install_app_from_dmg "Sequel Pro" "https://sequel-pro.googlecode.com/files/sequel-pro-1.0.2.dmg" "Sequel Pro 1.0.2"
+install_app_from_dmg "Slate" "http://slate.ninjamonkeysoftware.com/Slate.dmg"
+
 install_app_from_bz "FileZilla" "http://downloads.sourceforge.net/project/filezilla/FileZilla_Client/3.9.0.6/FileZilla_3.9.0.6_macosx-x86.app.tar.bz2"
 install_app_from_bz "Atom" "https://atom.io/download/mac"
+install_app_from_bz "iTerm" "https://iterm2.com/downloads/stable/iTerm2-2_1_1.zip"
+
 install_app_from_pkg "Docker" "https://github.com/docker/toolbox/releases/download/v1.8.2/DockerToolbox-1.8.2.pkg"
+
+install_by_homebrew "tree"
+install_by_homebrew "watch"
+install_by_homebrew "wget"
+install_by_homebrew "tmux"
+install_by_homebrew "nvm"
+
+install_rvm
+
+# zsh & oh_my_zsh
+install_by_homebrew "zsh"
+install_oh_my_zsh
+
+# vim & spf13
+install_by_homebrew "vim"
+# sudo ln -fs /usr/local/bin/vim /usr/bin/vi
+install_spf13

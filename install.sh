1be686df5d670f81a770224ae89f6bbf1c9ef29d
Kos Huang <kos@Kos-Pc.local>
2015-09-12 17:25:14 +0800

    extract functions

--- install.sh
+++ install.sh
@@ -1,84 +1,75 @@
 #!/bin/bash
 
-mkdir ~/getmacapps_temp
-cd ~/getmacapps_temp
+function install_app_from_dmg {
+  app=$1
+  url=$2
+  dmg_file="$app.dmg"
+  app_folder="/Applications/$app.app"
+  volumn_path="/Volumes/$app"
 
-# Installing Chrome
-echo Installing Chrome
-curl -L -O "https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg"
-hdiutil mount -nobrowse googlechrome.dmg
-cp -R "/Volumes/Google Chrome/Google Chrome.app" /Applications
-hdiutil unmount "/Volumes/Google Chrome"
-rm googlechrome.dmg
+  if [ -d "$app_folder" ]
+  then
+    echo "$app installed."
+  else
+    echo "Installing $app"
 
-# Installing Firefox
-echo Installing Firefox
-curl -L -o Firefox.dmg "http://download.mozilla.org/?product=firefox-latest&os=osx&lang=en-US"
-hdiutil mount -nobrowse Firefox.dmg
-cp -R "/Volumes/Firefox/Firefox.app" /Applications
-hdiutil unmount "/Volumes/Firefox"
-rm Firefox.dmg
+    # download dmg if it is not existed
+    if [ -e "$dmg_file" ]
+    then
+      echo "$dmg_file existed"
+    else
+      echo "download $dmg_file"
+      curl -L -o "$dmg_file" "$url"
+    fi
 
-# Installing Spotify
-echo Installing Spotify
-curl -L -O "http://download.spotify.com/Spotify.dmg"
-hdiutil mount -nobrowse Spotify.dmg
-cp -R "/Volumes/Spotify/Spotify.app" /Applications
-hdiutil unmount "/Volumes/Spotify"
-rm Spotify.dmg
+    hdiutil mount -nobrowse "$dmg_file"
+    cp -R "$volumn_path/$app.app" /Applications
+    hdiutil unmount "$volumn_path"
 
-# Installing Skype
-echo Installing Skype
-curl -L -O "http://www.skype.com/go/getskype-macosx.dmg"
-hdiutil mount -nobrowse getskype-macosx.dmg
-cp -R "/Volumes/Skype/Skype.app" /Applications
-hdiutil unmount "/Volumes/Skype"
-rm getskype-macosx.dmg
+    echo "$app_folder has been installed."
+  fi
+}
 
-# Installing Dropbox
-echo Installing Dropbox
-curl -L -o Dropbox.dmg "https://www.dropbox.com/download?plat=mac"
-hdiutil mount -nobrowse Dropbox.dmg
-cp -R "/Volumes/Dropbox Installer/Dropbox.app" /Applications
-hdiutil unmount "/Volumes/Dropbox Installer"
-rm Dropbox.dmg
+function install_app_from_bz {
+  app=$1
+  url=$2
+  volumn_name="${3:-$app}" # default as app
 
-# Installing Sublime Text 3 Beta
-echo Installing Sublime Text 3 Beta
-curl -L -o "Sublime Text 3.dmg" "http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%20Build%203065.dmg"
-hdiutil mount -nobrowse "Sublime Text 3.dmg"
-cp -R "/Volumes/Sublime Text/Sublime Text.app" /Applications
-hdiutil unmount "/Volumes/Sublime Text/Sublime Text.app"
-rm "Sublime Text 3.dmg"
+  bz_file="$app.app.tar.bz2"
+  app_folder="/Applications/$app.app"
+  volumn_path="/Volumes/$volumn_name"
 
-# Installing Sequel Pro
-echo Installing Sequel Pro
-curl -L -o sequel-pro.dmg "https://sequel-pro.googlecode.com/files/sequel-pro-1.0.2.dmg"
-hdiutil mount -nobrowse sequel-pro.dmg
-cp -R "/Volumes/Sequel Pro 1.0.2/Sequel Pro.app" /Applications
-hdiutil unmount "/Volumes/Sequel Pro 1.0.2"
-rm sequel-pro.dmg
+  if [ -d "$app_folder" ]
+  then
+    echo "$app installed."
+  else
+    echo "Installing $app"
 
-# Installing Source Tree
-echo Installing Source Tree
-curl -L -o SourceTree.dmg "http://downloads.atlassian.com/software/sourcetree/SourceTree_2.0.5.2.dmg"
-hdiutil mount -nobrowse SourceTree.dmg
-cp -R "/Volumes/SourceTree/SourceTree.app" /Applications
-hdiutil unmount "/Volumes/SourceTree"
-rm SourceTree.dmg
+    # download dmg if it is not existed
+    if [ -e "$bz_file" ]
+    then
+      echo "$bz_file existed"
+    else
+      echo "download $bz_file"
+      curl -L -o "$bz_file" "$url"
+    fi
 
-# Installing FileZilla
-echo Installing FileZilla
-curl -L -o Filezilla.app.tar.bz2 "http://downloads.sourceforge.net/project/filezilla/FileZilla_Client/3.9.0.6/FileZilla_3.9.0.6_macosx-x86.app.tar.bz2"
-tar -zxvf Filezilla.app.tar.bz2
-mv FileZilla.app /Applications
-rm Filezilla.app.tar.bz2
+    tar -zxvf "$app.app.tar.bz2"
+    mv "$app.app" /Applications
 
-# Installing Evernote
-echo Installing Evernote
-curl -L -o Evernote.dmg "http://evernote.com/download/get.php?file=EvernoteMac"
-yes | hdiutil mount -nobrowse Evernote.dmg -nobrowse > /dev/null
-cp -R "/Volumes/Evernote/Evernote.app" /Applications
-hdiutil unmount "/Volumes/Evernote"
-rm Evernote.dmg
+    echo "$app_folder has been installed."
+  fi
+}
 
+cd ~/Downloads
+
+install_app_from_dmg "Google Chrome" "https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg"
+install_app_from_dmg "FirefoxNightly" "https://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-trunk/firefox-43.0a1.en-US.mac.dmg"
+install_app_from_dmg "Spotify" "http://download.spotify.com/Spotify.dmg"
+install_app_from_dmg "Skype" "http://www.skype.com/go/getskype-macosx.dmg"
+install_app_from_dmg "Dropbox" "https://www.dropbox.com/download?plat=mac"
+install_app_from_dmg "Sublime Text" "http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%20Build%203065.dmg"
+install_app_from_dmg "SourceTree" "http://downloads.atlassian.com/software/sourcetree/SourceTree_2.0.5.2.dmg"
+install_app_from_dmg "Evernote" "http://evernote.com/download/get.php?file=EvernoteMac"
+install_app_from_dmg "Sequel Pro" "https://sequel-pro.googlecode.com/files/sequel-pro-1.0.2.dmg" "Sequel Pro 1.0.2"
+install_app_from_bz "FileZilla" "http://downloads.sourceforge.net/project/filezilla/FileZilla_Client/3.9.0.6/FileZilla_3.9.0.6_macosx-x86.app.tar.bz2"
